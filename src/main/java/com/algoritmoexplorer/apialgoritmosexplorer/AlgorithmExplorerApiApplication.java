a4e9f765105ba813c97f339eaac8d69d1b4bb73f
rpestana <marcos.pestana@tcema.tc.br>
2025-11-10 10:38:39 -0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/algoritmoexplorer/apialgoritmosexplorer/AlgorithmExplorerApiApplication.java
@@ -0,0 +1,13 @@
+package com.algoritmoexplorer.apialgoritmosexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmExplorerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmExplorerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algoritmoexplorer/apialgoritmosexplorer/AlgorithmExplorerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algoritmoexplorer.apialgoritmosexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmExplorerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
